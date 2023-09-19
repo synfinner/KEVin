b95602e3efd161efbd7f69445cc1ec85dcd0df77
synfinner <synfinner@synfinner.ru>
2023-09-18 20:11:11 -0400

    Stream KEV data #33, Connection pooling #34

--- update.py
+++ update.py
@@ -14,37 +14,33 @@ MONGO_URI = os.getenv("MONGODB_URI_PROD")
 DB_NAME = "kev"
 COLLECTION_NAME = "vulns"
 
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
 # URL for the JSON data
 DATA_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
 
-def get_data_from_url(url):
-    response = requests.get(url)
+def stream_data_from_url(url):
+    response = requests.get(url, stream=True)
     if response.status_code == 200:
-        return response.json()
+        data = response.json()  # Parse the entire JSON response
+        if "vulnerabilities" in data:
+            for vulnerability in data["vulnerabilities"]:
+                yield vulnerability
     else:
         print("Failed to fetch data from URL.")
-        return None
+        yield None  # Yield None to handle the error case
 
 def insert_data_to_mongodb(data):
-    client = MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
-
-    # Insert or update each vulnerability in the data
-    for vulnerability in data["vulnerabilities"]:
-        # Check if the cveID already exists in the database
-        # if it does, skip. If it does not exist, insert the vulnerability and print the newly inserted vulnerability
-        if collection.find_one({"cveID": vulnerability["cveID"]}):
-            #print(f"{vulnerability['cveID']} already exists in database. Skipping.")
-            continue
-        else:
+    for vulnerability in data:
+        if not collection.find_one({"cveID": vulnerability["cveID"]}):
             collection.insert_one(vulnerability)
             print(f"Inserted vulnerability {vulnerability['cveID']} into the database.")
-    client.close()
 
 if __name__ == "__main__":
-    # Fetch data from the URL
-    data = get_data_from_url(DATA_URL)
-    if data:
-        # Insert or update data in MongoDB
-        insert_data_to_mongodb(data)
+    # Stream and insert data
+    data_stream = stream_data_from_url(DATA_URL)
+    insert_data_to_mongodb(data_stream)
+
+client.close()
